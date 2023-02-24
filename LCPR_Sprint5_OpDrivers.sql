cafbfe256668e882e8f4ce6a4ef9597f8e8d647a
juanbeltran852 <125683314+juanbeltran852@users.noreply.github.com>
2023-02-24 12:34:38 -0500

    Update LCPR_Sprint5_OpDrivers.sql

--- LCPR_Sprint5_OpDrivers.sql
+++ LCPR_Sprint5_OpDrivers.sql
@@ -4,11 +4,11 @@ WITH
 
 --- ### Get the needed tables
 
-fixed_table as (
+ fixed_table as (
 SELECT * FROM "db_stage_dev"."lcpr_fixed_table_jan_feb23"
 ) --- WARNING: The table name will be updated, so keep the code with the right name.
  
-,interactions as (
+, interactions as (
 SELECT * FROM "lcpr.stage.prod"."lcpr_interactions_csg"
 )
     
@@ -23,7 +23,7 @@ SELECT * FROM "lcpr.stage.prod"."lcpr_interactions_csg"
      
 --- #### Reiterative tickets
 
-,initial_table as (
+, initial_table as (
 SELECT
     interaction_start_time,
     interaction_id,
@@ -33,7 +33,7 @@ SELECT
 FROM interactions
 )
 
-,tickets_count as (
+, tickets_count as (
 SELECT 
     Ticket_Month, 
     account_id, 
@@ -70,13 +70,106 @@ LEFT JOIN reiteractions_summary r
 
 --- #### Outlier repair times - using interactions
 
-repair_times as (
+, repair_times as (
 SELECT
-    date_trun('Month', date(interaction_start_time)) as Repair_Month, 
-    account_id as account, 
+    date_trunc('Month', date(interaction_start_time)) as Repair_Month, 
+    account_id, 
     interaction_start_time, 
     interaction_end_time, 
     date_diff('day', interaction_start_time, interaction_end_time) as solving_time
 FROM interactions
-WHERE   interaction_status = 'CLOSED'
+WHERE interaction_status = 'Closed'
 )
+-- solving_time is basically a column filled with 0s, is this right?
+
+, outlier_times as (
+SELECT
+    Repair_Month, 
+    account_id, 
+    interaction_start_time, 
+    interaction_end_time, 
+    solving_time, 
+    case when max(solving_time) > 4 then account_id else null end as outlier_repair
+FROM repair_times
+GROUP BY Repair_Month, account_id, interaction_start_time, interaction_end_time, solving_time
+ORDER BY Repair_Month desc, solving_time desc, account_id
+)
+
+, outlier_repair_flag as (
+SELECT
+    f.*, 
+    case when length(cast(f.fix_s_att_account as varchar)) = 8 then 'Cerillion' else 'Liberate' end as CRM, 
+    Repair_Month, 
+    case when account_id is not null then account_id else null end as techticket, 
+    outlier_repair
+FROM reiteractiontickets_flags f
+LEFT JOIN outlier_times o 
+    ON cast(f.fix_s_att_account as varchar) = cast(o.account_id as varchar) and f.fix_s_dim_month = o.Repair_Month
+)
+
+--- It works (24/2/2023 11:39am)
+
+--- #### Tech tickets density
+
+, tickets_per_account as (
+SELECT 
+    date_trunc('Month', date(interaction_start_time)) as Ticket_Month, 
+    account_id, 
+    count(distinct interaction_id) as num_tickets
+FROM interactions
+GROUP BY date_trunc('Month', date(interaction_start_time)), account_id
+)
+
+, records_fixed_accounts as (
+SELECT 
+    distinct fix_s_dim_month, 
+    fix_s_att_account, 
+    count(*) as num_records --- What about the use of distinct?
+FROM outlier_repair_flag
+WHERE fix_s_dim_month = date(fix_e_att_maxstart) --- Is this the right month choose? There is column called simply 'Month' as in Official CJW Sprint 5 code in GitLab.
+GROUP BY distinct fix_s_dim_month, fix_s_att_account
+)
+
+, ticket_density_flag as (
+SELECT 
+    f.*, 
+    num_tickets, 
+    num_records, 
+    (num_tickets/num_records) as adj_tickets
+FROM outlier_repair_flag f
+INNER JOIN records_fixed_accounts r
+    ON cast(f.fix_s_att_account as varchar) = cast(r.fix_s_att_account as varchar) and f.fix_s_dim_month = r.fix_s_dim_month
+LEFT JOIN tickets_per_account t
+    ON cast(f.fix_s_att_account as varchar) = cast(t.account_id as varchar) and f.fix_s_dim_month = t.Ticket_Month
+)
+
+-- , results_table_S5 as (
+-- SELECT
+--     Month, 
+--     E_Final_Tech_Flags, 
+--     E_FMC_Segment, 
+--     E_FMCType, 
+--     E_FinalTenureSegment, 
+--     count(distinct fixed_account) as activebase, 
+--     count(distinct one_tckt) as one_ticket, 
+--     count(distinct over1_tickt) as over1_ticket, 
+--     count(distinct two_tckt) as two_tickets, 
+--     count(distinct three_tckt) as three_more_tickets, 
+--     count(distinct techticket) as ticket_customers, 
+--     sum(adj_tickets) as total_tickets, 
+--     count(distinct outlier_repair) as outlier_repairs
+-- FROM ticket_density_flag
+-- WHERE
+--     finalchurnflag != 'Fixed Churner'
+--     and waterfall_flag != 'Downsell-Fixed Customer Gap'
+--     and waterfall_flag != 'Fixed Base Exceptions'
+--     and mainmovement != '6.Null last day'
+--     and waterfall_flag != 'Churn Exception'
+--     and month = date(dt)
+-- GROUP BY 1, 2, 3, 4, 5
+-- ORDER BY 1, 2, 3, 4, 5
+-- )
+
+--- 'Translate' this last results_table to PR columns names.
+
+SELECT * FROM ticket_density_flag
