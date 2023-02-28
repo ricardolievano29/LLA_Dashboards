1fea8fa5d0dc012bb1886bfea06b51fe4bfd26c9
juanbeltran852 <125683314+juanbeltran852@users.noreply.github.com>
2023-02-28 15:49:35 -0500

    Create LCPR_Sprint5_OpDrivers_Outlier_Repairs.sql

---
+++ LCPR_Sprint5_OpDrivers_Outlier_Repairs.sql
@@ -0,0 +1,162 @@
+--- ##### LCPR SPRINT 5  OPERATIONAL DRIVERS - REPAIR TIMES (OUTLIER REPAIRS) #####
+
+--- ### Initial steps
+
+WITH
+  
+ parameters as (
+ SELECT date_trunc('month', date('2023-01-01')) as input_month --- Input month you wish the code run for
+ )
+
+
+
+, fmc_table as ( --- This actually is the Fixed Table, it is called fmc just to get ready for when that table is ready
+SELECT
+    fix_s_dim_month, --- month
+    fix_b_fla_tech, --- B_Final_TechFlag
+    fix_b_fla_fmc, --- B_FMCSegment
+    fix_b_fla_mixcodeadj, --- B_FMCType
+    fix_e_fla_tech, --- E_Final_Tech_Flag
+    fix_e_fla_fmc, --- E_FMCSegment
+    fix_e_fla_mixcodeadj, --- E_FMCType
+    fix_b_fla_tenure, -- b_final_tenure
+    fix_e_fla_tenure, --- e_final_tenure
+    --- B_FixedTenure
+    --- E_FixedTenure
+    --- finalchurnflag
+    fix_s_fla_churntype, --- fixedchurntype
+    fix_s_fla_churnflag, --- fixedchurnflag
+    fix_s_fla_mainmovement, --- fixedmainmovement
+    --- waterfall_flag
+    --- finalaccount
+    fix_s_att_account, -- fixedaccount
+    fix_e_att_active --- f_activebom
+    --- mobile_activeeom
+    --- mobilechurnflag
+FROM "db_stage_dev"."lcpr_fixed_table_jan_feb28" --- Keep this updated to the lastest version!
+WHERE 
+    fix_s_dim_month = (SELECT input_month FROM parameters)
+)
+
+, repeated_accounts as (
+SELECT 
+    fix_s_dim_month, 
+    fix_s_att_account, 
+    count(*) as records_per_user
+FROM fmc_table
+GROUP BY 1, 2
+ORDER BY 3 desc
+)
+
+, fmc_table_adj as (
+SELECT 
+    F.*,
+    records_per_user
+FROM fmc_table F
+LEFT JOIN repeated_accounts R
+    ON F.fix_s_att_account = R.fix_s_att_account and F.fix_s_dim_month = R.fix_s_dim_month
+)
+
+, clean_interaction_time as (
+SELECT *
+FROM "lcpr.stage.prod"."lcpr_interactions_csg"
+WHERE
+    (cast(interaction_start_time as varchar) != ' ') 
+    and (interaction_start_time is not null)
+    and date_trunc('month', cast(substr(cast(interaction_start_time as varchar),1,10) as date)) between ((SELECT input_month FROM parameters)) and ((SELECT input_month FROM parameters) + interval '1' month)
+        
+)
+
+, interactions_fields as (
+SELECT
+    *, 
+    cast(substr(cast(interaction_start_time as varchar), 1, 10) as date) as interaction_date, 
+    date_trunc('month', cast(substr(cast(interaction_start_time as varchar), 1, 10) as date)) as month
+FROM clean_interaction_time
+)
+
+--- ### Repair times
+
+, repair_times as (
+SELECT
+    account_id, 
+    cast(substr(cast(interaction_start_time as varchar), 1, 10) as date) as interaction_start_time, 
+    cast(substr(cast(interaction_end_time as varchar), 1, 10) as date) as interaction_end_time, 
+    date_diff('day', cast(substr(cast(interaction_start_time as varchar), 1, 10) as date), cast(substr(cast(interaction_end_time as varchar), 1, 10) as date)) as duration,
+    date_trunc('day', cast(substr(cast(interaction_start_time as varchar), 1, 10) as date)) as month
+FROM clean_interaction_time
+WHERE
+    interaction_purpose_descrip in ('Antivirus Calls', 'Bloq Red Wifi', 'Bloqueo Red Wifi', 'Cable Card Install', 'Cable Problem', 'Call Fwd Activaci??n', 'Call Fwd Activacin', 'Call Fwd Activacion', 'Call Fwd Desactivar', 'Cambio Ssid', 'Ci: Cable Card Req', 'Commercial Accounts', 'Configuration', 'Cs: Change Name(fco)', 'Cs: Offer Correction', 'Cs: Portability', 'Email Issues', 'Eq: Deprogrammed', 'Eq: Intermittence', 'Eq: Lost', 'Eq: Lost/Stolen', 'Eq: No Conection', 'Eq: No Start', 'Eq: Not Working', 'Eq: Notif Letter', 'Eq: Pixels', 'Eq: Ref. By Csr', 'Eq: Replace Remote', 'Eq: Return Equip.', 'Eq: Up/Dwn/Side', 'Eq: Up/Side', 'Fast Busy Tone', 'Functions Oriented', 'G:disconect Warning', 'G:follow Up', 'G:order Entry Error', 'Headend Issues', 'Headend Power Outage', 'HSD Intermittent', 'HSD Issues', 'HSD No Browsing', 'HSD Problem', 'HSD Slow Service', 'Ibbs-Ip Issues', 'Integra 5', 'Internet Calls', 'Lnp Complete', 'Lnp In Process', 'Lnp Not Complete', 'Mcafee', 'Mi Liberty Problem', 'Nc Busy Tone', 'Nc Cancel Job', 'Nc Message V. Mail', 'Nc No Answer', 'Nc Ok Cust Confirmed', 'Nc Rescheduled', 'Nc Wrong Phone No.', 'No Browse', 'No Retent Relate', 'No Retention Call', 'No Service All', 'Non Pay', 'Np: Restard Svc Only', 'Nret- Contract', 'Nret- Diss Cust Serv', 'Nret- Equipment', 'Nret- Moving', 'Nret- No Facilities', 'Outages', 'Phone Cant Make Call', 'Phone Cant Recv Call', 'Phone No Tone', 'PPV Order', 'PPV/Vod Problem', 'Reconnection', 'Refered Same Day', 'Restard Svc Only', 'Restart 68-69 Days', 'Restart Svc Only', 'Ret-Serv Education', 'Ret-Sidegrade', 'Ret-Upgrade', 'Retained Customer', 'Retent Effort Call', 'Retention', 'Return Mail', 'Rt: Dowgrde Convert', 'Rt: Dowgrde Premium', 'Schd Appiont 4 Tech', 'Self-Inst Successful', 'Self-Install', 'Self-Install N/A', 'Self-Int Rejected', 'Service Techs', 'Sl: Advanced Prod.', 'Sl: Install/Cos Conf', 'Sl: Outbound Sale', 'Sl: Product Info', 'Sl: Restart', 'Sl: Upg Addon/Tiers', 'Sl: Upg Events', 'Sl: Upg Service', 'Sl: Upgrade Tn', 'Sol Contrasena Wifi', 'Solicitud Contrasena', 'Solicitud Num Cuent', 'Solicitud Num Cuenta', 'Sp: Aee-No Liberty', 'Vd: Tech.Service', 'Video Issues', 'Video Problem', 'Video Programming', 'Voice Issues', 'Voice Outages', 'Wifi Password', 'Work Order Status')
+    and interaction_status = 'Closed'
+)
+
+--- ### Repair times flag
+
+, repair_times_flag as (
+SELECT 
+    F.*, 
+    case when duration >= 4 then fix_s_att_account else null end as outlier_repair
+FROM fmc_table_adj F
+LEFT JOIN repair_times I
+    ON cast(F.fix_s_att_account as varchar) = cast(I.account_id as varchar) and F.fix_s_dim_month = I.month
+)
+
+, final_fields as (
+SELECT
+    distinct fix_s_dim_month, -- month
+    fix_b_fla_tech, -- B_Final_TechFlag
+    fix_b_fla_fmc, -- B_FMCSegment
+    fix_b_fla_mixcodeadj, -- B_FMCType
+    fix_e_fla_tech, -- E_Final_TechFlag
+    fix_e_fla_fmc, -- E_FMCSegment
+    fix_e_fla_mixcodeadj, -- E_FMCType
+    -- b_final_tenure
+    -- e_final_tenure
+    fix_b_fla_tenure, -- B_FixedTenure
+    fix_e_fla_tenure, -- E_FixedTenure
+    -- finalchurnflag
+    -- fixedchurnflag
+    fix_s_fla_churntype, -- fixedchurntype
+    fix_s_fla_mainmovement, -- fixedmainmovement
+    -- waterfall_flag
+    -- mobile_activeeom
+    -- mobilechurnflag
+    -- finalaccount
+    fix_s_att_account, -- fixedaccount
+    records_per_user, 
+    outlier_repair
+FROM repair_times_flag
+)
+
+SELECT
+    fix_s_dim_month, -- month
+    fix_b_fla_tech, -- B_Final_TechFlag
+    fix_b_fla_fmc, -- B_FMCSegment
+    fix_b_fla_mixcodeadj, -- B_FMCType
+    fix_e_fla_tech, -- E_Final_TechFlag
+    fix_e_fla_fmc, -- E_FMCSegment
+    fix_e_fla_mixcodeadj, -- E_FMCType
+    -- b_final_tenure
+    -- e_final_tenure
+    fix_b_fla_tenure, -- B_FixedTenure
+    fix_e_fla_tenure, -- E_FixedTenure
+    -- finalchurnflag
+    -- fixedchurnflag
+    fix_s_fla_churntype, -- fixedchurntype
+    fix_s_fla_mainmovement, -- fixedmainmovement
+    -- waterfall_flag
+    -- mobile_activeeom
+    -- mobilechurnflag
+    -- finalaccount
+    fix_s_att_account, -- fixedaccount
+    records_per_user,
+    count(distinct fix_s_att_account) as Total_Accounts,
+    count(distinct fix_s_att_account) as Fixed_Accounts, 
+    count(distinct outlier_repair) as outlier_repairs
+FROM final_fields
+-- WHERE ((fix_s_fla_churntype != '2. Fixed Involuntary Churner' and fix_s_fla_churntype != '1. Fixed Voluntary Churner') or fix_s_fla_churntype is null) and fix_s_fla_churntype != 'Fixed Churner'
+GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13
+
+--- ### Specific numbers
+
+-- SELECT count(distinct fix_s_att_account) FROM clean_interaction_time WHERE oulier_repairs is not null and outlier_repairs != 0 and outlier_repairs != '0'
