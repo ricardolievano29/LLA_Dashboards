64ad66cda60782e38b05bf297e210f33a0a600b9
juanbeltran852 <125683314+juanbeltran852@users.noreply.github.com>
2023-03-03 01:05:56 -0500

    Update LCPR_Sprint5_OpDrivers_Tickets_per_Month_V3.sql

--- Tickets_per_Month/LCPR_Sprint5_OpDrivers_Tickets_per_Month_V3.sql
+++ Tickets_per_Month/LCPR_Sprint5_OpDrivers_Tickets_per_Month_V3.sql
@@ -70,9 +70,11 @@ WHERE
 , interactions_fields as (
 SELECT
     *,
+    --- Option 1: interaction_start_time
     cast(substr(cast(interaction_start_time as varchar), 1, 10) as date) as interaction_date, 
     date_trunc('month', cast(substr(cast(interaction_start_time as varchar), 1, 10) as date)) as month
 FROM clean_interaction_time
+WHERE account_id = '8211790320579737' --- Just for experiments
 )
 
 --- ### Tickets per month
@@ -81,9 +83,9 @@ FROM clean_interaction_time
 SELECT
     distinct account_id, 
     interaction_id, 
-    interaction_date ---  Is this the right way to count tickets????
+    interaction_date
 FROM interactions_fields
-WHERE interaction_purpose_descrip in ( --- There may be a problem identifying tickets with interaction_purpose_descrip!!!
+WHERE interaction_purpose_descrip in ( --- There may be a problem identifying tickets with interaction_purpose_descrip -> Outages considerations
     --- First tickets
     'Antivirus Calls', 'Bloq Red Wifi', 'Bloqueo Red Wifi', 'Cable Card Install', 'Cable Problem', 'Call Fwd Activaci??n', 'Call Fwd Activacin', 'Call Fwd Activacion', 'Call Fwd Desactivar', 'Cambio Ssid', 'Ci: Cable Card Req', 'Commercial Accounts', 'Configuration', 'Cs: Change Name(fco)', 'Cs: Offer Correction', 'Cs: Portability', 'Email Issues', 'Eq: Deprogrammed', 'Eq: Intermittence', 'Eq: Lost', 'Eq: Lost/Stolen', 'Eq: No Conection', 'Eq: No Start', 'Eq: Not Working', 'Eq: Notif Letter', 'Eq: Pixels', 'Eq: Ref. By Csr', 'Eq: Replace Remote', 'Eq: Return Equip.', 'Eq: Up/Dwn/Side', 'Eq: Up/Side', 'Fast Busy Tone', 'Functions Oriented', 'G:disconect Warning', 'G:follow Up', 'G:order Entry Error', 'Headend Issues', 'Headend Power Outage', 'HSD Intermittent', 'HSD Issues', 'HSD No Browsing', 'HSD Problem', 'HSD Slow Service', 'Ibbs-Ip Issues', 'Integra 5', 'Internet Calls', 'Lnp Complete', 'Lnp In Process', 'Lnp Not Complete', 'Mcafee', 'Mi Liberty Problem', 'Nc Busy Tone', 'Nc Cancel Job', 'Nc Message V. Mail', 'Nc No Answer', 'Nc Ok Cust Confirmed', 'Nc Rescheduled', 'Nc Wrong Phone No.', 'No Browse', 'No Retent Relate', 'No Retention Call', 'No Service All', 'Non Pay', 'Np: Restard Svc Only', 'Nret- Contract', 'Nret- Diss Cust Serv', 'Nret- Equipment', 'Nret- Moving', 'Nret- No Facilities', 'Outages', 'Phone Cant Make Call', 'Phone Cant Recv Call', 'Phone No Tone', 'PPV Order', 'PPV/Vod Problem', 'Reconnection', 'Refered Same Day', 'Restard Svc Only', 'Restart 68-69 Days', 'Restart Svc Only', 'Ret-Serv Education', 'Ret-Sidegrade', 'Ret-Upgrade', 'Retained Customer', 'Retent Effort Call', 'Retention', 'Return Mail', 'Rt: Dowgrde Convert', 'Rt: Dowgrde Premium', 'Schd Appiont 4 Tech', 'Self-Inst Successful', 'Self-Install', 'Self-Install N/A', 'Self-Int Rejected', 'Service Techs', 'Sl: Advanced Prod.', 'Sl: Install/Cos Conf', 'Sl: Outbound Sale', 'Sl: Product Info', 'Sl: Restart', 'Sl: Upg Addon/Tiers', 'Sl: Upg Events', 'Sl: Upg Service', 'Sl: Upgrade Tn', 'Sol Contrasena Wifi', 'Solicitud Contrasena', 'Solicitud Num Cuent', 'Solicitud Num Cuenta', 'Sp: Aee-No Liberty', 'Vd: Tech.Service', 'Video Issues', 'Video Problem', 'Video Programming', 'Voice Issues', 'Voice Outages', 'Wifi Password', 'Work Order Status', 
     --- Now truckrolls
@@ -96,7 +98,7 @@ SELECT
     account_id, 
     --- ### Option 1: distinct interaction_date
     count(distinct interaction_date) as number_tickets
-    --- ### Option 2: Non-distinct interaction_date (defined previously)
+    --- ### Option 2: Non-distinct interaction_date
     -- count(interaction_date) as number_tickets
 FROM users_tickets
 WHERE interaction_id is not null
@@ -142,7 +144,7 @@ FROM number_tickets_flag
 WHERE fix_s_fla_churnflag = '2. Fixed NonChurner'
 )
 
--- SELECT
+SELECT
     fix_s_dim_month, -- month
     fix_b_fla_tech, -- B_Final_TechFlag
     fix_b_fla_fmc, -- B_FMCSegment
@@ -161,7 +163,6 @@ WHERE fix_s_fla_churnflag = '2. Fixed NonChurner'
     count(distinct fix_s_att_account) as Fixed_Accounts, 
     sum(number_tickets) as number_tickets
 FROM final_fields
--- WHERE fix_s_fla_churnflag = '2. Fixed NonChurner'
 GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9
 
 -- ### Specific numbers
@@ -169,8 +170,8 @@ GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9
 --- Number of clients per tickets
 -- SELECT distinct number_tickets, count(distinct fix_s_att_account) FROM final_fields GROUP BY 1 ORDER BY 1 asc, 2 desc
 
---- Top 10 clients with most tickets
--- SELECT fix_s_att_account, number_tickets FROM final_fields ORDER BY 2 desc
+--- Top 5 clients with most tickets
+-- SELECT fix_s_att_account, number_tickets FROM final_fields ORDER BY 2 desc LIMIT 5
 
 --- Deep dive per account (interactions)
 -- SELECT
@@ -187,6 +188,11 @@ GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9
 -- FROM interactions_fields
 -- WHERE 
 --     account_id = '8211790320579737'
+--     and interaction_purpose_descrip in (
+--     --- First tickets
+--     'Antivirus Calls', 'Bloq Red Wifi', 'Bloqueo Red Wifi', 'Cable Card Install', 'Cable Problem', 'Call Fwd Activaci??n', 'Call Fwd Activacin', 'Call Fwd Activacion', 'Call Fwd Desactivar', 'Cambio Ssid', 'Ci: Cable Card Req', 'Commercial Accounts', 'Configuration', 'Cs: Change Name(fco)', 'Cs: Offer Correction', 'Cs: Portability', 'Email Issues', 'Eq: Deprogrammed', 'Eq: Intermittence', 'Eq: Lost', 'Eq: Lost/Stolen', 'Eq: No Conection', 'Eq: No Start', 'Eq: Not Working', 'Eq: Notif Letter', 'Eq: Pixels', 'Eq: Ref. By Csr', 'Eq: Replace Remote', 'Eq: Return Equip.', 'Eq: Up/Dwn/Side', 'Eq: Up/Side', 'Fast Busy Tone', 'Functions Oriented', 'G:disconect Warning', 'G:follow Up', 'G:order Entry Error', 'Headend Issues', 'Headend Power Outage', 'HSD Intermittent', 'HSD Issues', 'HSD No Browsing', 'HSD Problem', 'HSD Slow Service', 'Ibbs-Ip Issues', 'Integra 5', 'Internet Calls', 'Lnp Complete', 'Lnp In Process', 'Lnp Not Complete', 'Mcafee', 'Mi Liberty Problem', 'Nc Busy Tone', 'Nc Cancel Job', 'Nc Message V. Mail', 'Nc No Answer', 'Nc Ok Cust Confirmed', 'Nc Rescheduled', 'Nc Wrong Phone No.', 'No Browse', 'No Retent Relate', 'No Retention Call', 'No Service All', 'Non Pay', 'Np: Restard Svc Only', 'Nret- Contract', 'Nret- Diss Cust Serv', 'Nret- Equipment', 'Nret- Moving', 'Nret- No Facilities', 'Outages', 'Phone Cant Make Call', 'Phone Cant Recv Call', 'Phone No Tone', 'PPV Order', 'PPV/Vod Problem', 'Reconnection', 'Refered Same Day', 'Restard Svc Only', 'Restart 68-69 Days', 'Restart Svc Only', 'Ret-Serv Education', 'Ret-Sidegrade', 'Ret-Upgrade', 'Retained Customer', 'Retent Effort Call', 'Retention', 'Return Mail', 'Rt: Dowgrde Convert', 'Rt: Dowgrde Premium', 'Schd Appiont 4 Tech', 'Self-Inst Successful', 'Self-Install', 'Self-Install N/A', 'Self-Int Rejected', 'Service Techs', 'Sl: Advanced Prod.', 'Sl: Install/Cos Conf', 'Sl: Outbound Sale', 'Sl: Product Info', 'Sl: Restart', 'Sl: Upg Addon/Tiers', 'Sl: Upg Events', 'Sl: Upg Service', 'Sl: Upgrade Tn', 'Sol Contrasena Wifi', 'Solicitud Contrasena', 'Solicitud Num Cuent', 'Solicitud Num Cuenta', 'Sp: Aee-No Liberty', 'Vd: Tech.Service', 'Video Issues', 'Video Problem', 'Video Programming', 'Voice Issues', 'Voice Outages', 'Wifi Password', 'Work Order Status', 
+--     --- Now truckrolls
+--         'Ci: Inst/Tc Status', 'Ci: Install Stat', 'Ci: Installer / Tech', 'Create Trouble Call', 'Cs: Transfer', 'Dialtone/Line Issues', 'Eq: Not Recording', 'Eq: Port Damage', 'Eq: Ref By Tech/Inst', 'Eq: Vod No Access', 'Eq:error E1:26 E1:36', 'Equipment Problem', 'Equipment Swap', 'Fiber Outages', 'Maintenance Techs', 'Provision/Contractor', 'Sl: New Sales', 'Sl: Upgrade HSD A/O', 'Sp: Already Had Tc', 'Sp: Cancelled Tc', 'Sp: Drops Issues', 'Sp: HSD-Intermit.', 'Sp: HSD-No Browse', 'Sp: HSD-No Connect', 'Sp: HSD-Speed Issues', 'Sp: No Signal-3 Pack', 'Sp: Pending Mr-Sro', 'Sp: Poste Ca?-do', 'Sp: Poste Cado', 'Sp: PPV', 'Sp: Precortes Issues', 'Sp: Recent Install', 'Sp: Referred To Noc', 'Sp: Tel-Cant Make', 'Sp: Tel-Cant Receive', 'Sp: Tel-No Tone', 'Sp: Video-Intermit.', 'Sp: Video-No Signal', 'Sp: Video-Tiling', 'Sp: Vod', 'Sp:hsd- Ip Issues', 'Sp:hsd- Wifi Issues', 'Sp:tc/Mr Confirm', 'Sp:tel- Voice Mail', 'Status Of Install', 'Status/Trouble Calls', 'Tel Issues', 'Tel Problem', 'Telephony Calls', 'Transfer', 'Vd: Transferred')
 -- ORDER BY 2, 3, 4, 5, 6, 7, 8, 9, 10
 
 --- KPI calculation
