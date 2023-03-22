37b90210cdeabc8ccc758331d691aa556f543906
juanbeltran852 <125683314+juanbeltran852@users.noreply.github.com>
2023-03-21 22:31:26 -0500

    Update LCPR_Sprint5_OpDrivers_Truckrolls_IssueSolved.sql

--- Tickets_per_Month/LCPR_Sprint5_OpDrivers_Truckrolls_IssueSolved.sql
+++ Tickets_per_Month/LCPR_Sprint5_OpDrivers_Truckrolls_IssueSolved.sql
@@ -1,9 +1,8 @@
-
 WITH
 parameters as (
 SELECT date_trunc('month', date('2023-01-01')) as input_month)
 
-, table_part1 as (
+, truckrolls_part1 as (
 SELECT 
     create_dte_ojb, 
     job_no_ojb, 
@@ -12,7 +11,7 @@ FROM "lcpr.stage.dev"."truckrolls"
 WHERE substr(create_dte_ojb, 1, 1) != '"'
 )
 
-, table_part2 as (
+, truckrolls_part2 as (
 SELECT
     substr(create_dte_ojb, 2, 10) as create_dte_ojb, 
     job_no_ojb, 
@@ -21,19 +20,23 @@ FROM "lcpr.stage.dev"."truckrolls"
 WHERE substr(create_dte_ojb, 1, 1) = '"'
 )
 
-, tables_union as (
+, truckrolls_union as (
 SELECT
     create_dte_ojb, 
     job_no_ojb, 
     sub_acct_no_sbb
-FROM table_part1
-UNION ALL (SELECT * FROM table_part2)
+FROM truckrolls_part1
+UNION ALL (SELECT * FROM truckrolls_part2)
 )
 
+, truckrolls_clean as (
 SELECT
     *
-FROM tables_union
+FROM truckrolls_union
 WHERE
     cast(job_no_ojb as varchar) not in ('',  ' ') 
     and job_no_ojb is not null
     and date_trunc('month', date(create_dte_ojb)) = (SELECT input_month FROM parameters)
+)
+
+SELECT * FROM truckrolls_clean
