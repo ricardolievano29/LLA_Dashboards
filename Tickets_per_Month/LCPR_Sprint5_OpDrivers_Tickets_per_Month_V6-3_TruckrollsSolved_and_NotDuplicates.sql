2330d416eae49239119e542c6e70625378a0507a
juanbeltran852 <125683314+juanbeltran852@users.noreply.github.com>
2023-03-22 15:53:09 -0500

    Update LCPR_Sprint5_OpDrivers_Tickets_per_Month_V6-3_TruckrollsSolved_and_NotDuplicates.sql

--- Tickets_per_Month/LCPR_Sprint5_OpDrivers_Tickets_per_Month_V6-3_TruckrollsSolved_and_NotDuplicates.sql
+++ Tickets_per_Month/LCPR_Sprint5_OpDrivers_Tickets_per_Month_V6-3_TruckrollsSolved_and_NotDuplicates.sql
@@ -191,15 +191,14 @@ SELECT
         and lower(interaction_purpose_descrip) not like '%discon%serv%'
         and lower(interaction_purpose_descrip) not like '%serv%call%'
         )
-        
         then interaction_id else null
     end as techticket_flag,
     cast(job_no_ojb as varchar) as truckroll_flag
 FROM interactions_fields2 a
 LEFT JOIN truckrolls_clean b
     ON a.interaction_date = cast(create_dte_ojb as date) and cast(a.account_id as varchar) = cast(b.sub_acct_no_sbb as varchar)
--- WHERE
-    -- interaction_purpose_descrip not in ('Work Order Status', 'Default Call Wrapup', 'G:outbound Calls', 'Eq: Cust. First', 'Eq: Audit', 'Eq: Code Error', 'Downgrade Service', 'Disconnect Service', 'Rt: Dowgrde Service', 'Cust Service Calls')
+WHERE
+    interaction_purpose_descrip not in ('Work Order Status', 'Default Call Wrapup', 'G:outbound Calls', 'Eq: Cust. First', 'Eq: Audit', 'Eq: Code Error', 'Downgrade Service', 'Disconnect Service', 'Rt: Dowgrde Service', 'Cust Service Calls')
     -- and cast(job_no_ojb as varchar) not in ('',  ' ') 
     -- and job_no_ojb is not null
     -- and date_trunc('month', date(create_dte_ojb)) = (SELECT input_month FROM parameters)
