6bd29b41cbbdd99d98324052a93e2a41375b4e53
juanbeltran852 <125683314+juanbeltran852@users.noreply.github.com>
2023-03-21 20:56:44 -0500

    Update LCPR_Sprint5_OpDrivers_Truckrolls_IssueSolved.sql

--- Tickets_per_Month/LCPR_Sprint5_OpDrivers_Truckrolls_IssueSolved.sql
+++ Tickets_per_Month/LCPR_Sprint5_OpDrivers_Truckrolls_IssueSolved.sql
@@ -1,36 +1,39 @@
 
-SELECT
-count(*), 
-count(distinct job_no_ojb)
-FROM(
-SELECT 
--- create_dte_ojb, 
--- job_no_ojb
+WITH
+parameters as (
+SELECT date_trunc('month', date('2023-01-01')) as input_month)
 
--- count(*), 
-create_dte_ojb, 
-job_no_ojb, 
-sub_acct_no_sbb
-
-FROM
-(
+, table_part1 as (
 SELECT 
-create_dte_ojb, 
-job_no_ojb, 
-sub_acct_no_sbb
+    create_dte_ojb, 
+    job_no_ojb, 
+    sub_acct_no_sbb
 FROM "lcpr.stage.dev"."truckrolls" 
 WHERE substr(create_dte_ojb, 1, 1) != '"'
 )
-UNION ALL
-(
+
+, table_part2 as (
 SELECT
-substr(create_dte_ojb, 2, 10) as create_dte_ojb, 
-job_no_ojb, 
-sub_acct_no_sbb
+    substr(create_dte_ojb, 2, 10) as create_dte_ojb, 
+    job_no_ojb, 
+    sub_acct_no_sbb
 FROM "lcpr.stage.dev"."truckrolls"
 WHERE substr(create_dte_ojb, 1, 1) = '"'
 )
+
+, tables_union as (
+SELECT
+    create_dte_ojb, 
+    job_no_ojb, 
+    sub_acct_no_sbb
+FROM table_part1
+UNION ALL (SELECT * FROM table_part2)
 )
-WHERE 
-    cast(job_no_ojb as varchar) not in ('',  ' ') and job_no_ojb is not null
-    and date_trunc('month', date(create_dte_ojb)) = date_trunc('month', date('2023-02-01'))
+
+SELECT
+    *
+FROM tables_union
+WHERE
+    cast(job_no_ojb as varchar) not in ('',  ' ') 
+    and job_no_ojb is not null
+    and date_trunc('month', date(create_dte_ojb)) = (SELECT input_month FROM parameters)
