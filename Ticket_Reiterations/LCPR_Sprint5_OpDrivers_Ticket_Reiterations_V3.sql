5222f3ce99c79821e0f51d747cd84c8daad2bb33
juanbeltran852 <125683314+juanbeltran852@users.noreply.github.com>
2023-03-23 17:19:44 -0500

    Create LCPR_Sprint5_OpDrivers_Ticket_Reiterations_V3.sql

---
+++ Ticket_Reiterations/LCPR_Sprint5_OpDrivers_Ticket_Reiterations_V3.sql
@@ -0,0 +1,385 @@
+--- ##### LCPR SPRINT 5  OPERATIONAL DRIVERS - TICKET REITERATIONS #####
+--- ### Initial steps
+WITH
+  
+ parameters as (
+ SELECT date_trunc('month', date('2023-01-01')) as input_month --- Input month you wish the code run for
+ )
+ 
+, fmc_table as ( --- This actually is the Fixed Table, it is called fmc just to get ready for when that table is ready
+SELECT
+    fix_s_dim_month, --- month
+    fix_b_fla_tech, --- B_Final_TechFlag
+    fix_b_fla_fmc, --- B_FMCSegment
+    fix_b_fla_mixcodeadj, --- B_FMCType
+    fix_e_fla_tech, --- E_Final_Tech_Flag
+    fix_e_fla_fmc, --- E_FMCSegment
+    fix_e_fla_mixcodeadj, --- E_FMCType
+    fix_b_fla_tenure, -- b_final_tenure
+    fix_e_fla_tenure, --- e_final_tenure
+    --- B_FixedTenure
+    --- E_FixedTenure
+    --- finalchurnflag
+    fix_s_fla_churntype, --- fixedchurntype
+    fix_s_fla_churnflag, --- fixedchurnflag
+    fix_s_fla_mainmovement, --- fixedmainmovement
+    --- waterfall_flag
+    --- finalaccount
+    fix_s_att_account, -- fixedaccount
+    fix_e_att_active --- f_activebom
+    --- mobile_activeeom
+    --- mobilechurnflag
+FROM "db_stage_dev"."lcpr_fixed_table_jan_mar17" --- Keep this updated to the latest version!
+WHERE 
+    fix_s_dim_month = (SELECT input_month FROM parameters)
+    and fix_e_att_active = 1
+)
+
+, repeated_accounts as (
+SELECT 
+    fix_s_dim_month, 
+    fix_s_att_account, 
+    count(*) as records_per_user
+FROM fmc_table
+GROUP BY 1, 2
+ORDER BY 3 desc
+)
+
+, fmc_table_adj as (
+SELECT 
+    F.*,
+    records_per_user
+FROM fmc_table F
+LEFT JOIN repeated_accounts R
+    ON F.fix_s_att_account = R.fix_s_att_account and F.fix_s_dim_month = R.fix_s_dim_month
+)
+
+, clean_interaction_time as (
+SELECT *
+FROM "lcpr.stage.prod"."lcpr_interactions_csg"
+WHERE
+    cast(interaction_start_time as varchar) != ' ' 
+    and interaction_start_time is not null
+    and date_trunc('month', cast(substr(cast(interaction_start_time as varchar),1,10) as date)) between ((SELECT input_month FROM parameters)) and ((SELECT input_month FROM parameters) + interval '1' month)
+    and account_type = 'RES'
+)
+
+, interactions_fields as (
+SELECT
+    *, 
+    cast(substr(cast(interaction_start_time as varchar), 1, 10) as date) as interaction_date, 
+    date_trunc('month', cast(substr(cast(interaction_start_time as varchar), 1, 10) as date)) as month
+FROM clean_interaction_time
+)
+
+, interactions_not_repeated as (
+SELECT
+    first_value(interaction_id) OVER(PARTITION BY account_id, interaction_date, interaction_channel, interaction_agent_id, interaction_purpose_descrip ORDER BY interaction_date DESC) AS interaction_id2
+FROM interactions_fields
+)
+
+, interactions_fields2 as (
+SELECT *
+FROM interactions_not_repeated a
+LEFT JOIN interactions_fields b
+    ON a.interaction_id2 = b.interaction_id
+)
+
+, truckrolls_part1 as (
+SELECT 
+    create_dte_ojb, 
+    job_no_ojb, 
+    sub_acct_no_sbb
+FROM "lcpr.stage.dev"."truckrolls" 
+WHERE substr(create_dte_ojb, 1, 1) != '"'
+)
+
+, truckrolls_part2 as (
+SELECT
+    substr(create_dte_ojb, 2, 10) as create_dte_ojb, 
+    job_no_ojb, 
+    sub_acct_no_sbb
+FROM "lcpr.stage.dev"."truckrolls"
+WHERE substr(create_dte_ojb, 1, 1) = '"'
+)
+
+, truckrolls_union as (
+SELECT
+    create_dte_ojb, 
+    job_no_ojb, 
+    sub_acct_no_sbb
+FROM truckrolls_part1
+UNION ALL (SELECT * FROM truckrolls_part2)
+)
+
+, truckrolls_clean as (
+SELECT *
+FROM truckrolls_union
+WHERE
+    cast(job_no_ojb as varchar) not in ('',  ' ') 
+    and job_no_ojb is not null
+    and date_trunc('month', date(create_dte_ojb)) = (SELECT input_month FROM parameters)
+)
+
+--- ### Reiterative tickets
+
+, users_tickets_pre as (
+SELECT
+    distinct account_id, 
+    interaction_id, 
+    interaction_date, 
+    case when (
+        lower(interaction_purpose_descrip) like '%ppv%problem%'
+        or lower(interaction_purpose_descrip) like '%hsd%problem%'
+        or lower(interaction_purpose_descrip) like '%cable%problem%'
+        or lower(interaction_purpose_descrip) like '%tv%problem%'
+        or lower(interaction_purpose_descrip) like '%video%problem%'
+        or lower(interaction_purpose_descrip) like '%tel%problem%'
+        or lower(interaction_purpose_descrip) like '%phone%problem%'
+        or lower(interaction_purpose_descrip) like '%int%problem%'
+        or lower(interaction_purpose_descrip) like '%line%problem%'
+        or lower(interaction_purpose_descrip) like '%hsd%issue%'
+        or lower(interaction_purpose_descrip) like '%ppv%issue%'
+        or lower(interaction_purpose_descrip) like '%video%issue%'
+        or lower(interaction_purpose_descrip) like '%tel%issue%'
+        or lower(interaction_purpose_descrip) like '%phone%issue%'
+        or lower(interaction_purpose_descrip) like '%int%issue%'
+        or lower(interaction_purpose_descrip) like '%line%issue%'
+        or lower(interaction_purpose_descrip) like '%cable%issue%'
+        or lower(interaction_purpose_descrip) like '%tv%issue%'
+        or lower(interaction_purpose_descrip) like '%bloq%'
+        or lower(interaction_purpose_descrip) like '%slow%'
+        or lower(interaction_purpose_descrip) like '%slow%service%'
+        or lower(interaction_purpose_descrip) like '%service%tech%'
+        or lower(interaction_purpose_descrip) like '%tech%service%'
+        or lower(interaction_purpose_descrip) like '%no%service%'
+        or lower(interaction_purpose_descrip) like '%hsd%no%'
+        or lower(interaction_purpose_descrip) like '%hsd%slow%'
+        or lower(interaction_purpose_descrip) like '%hsd%intermit%'
+        or lower(interaction_purpose_descrip) like '%no%brows%'
+        or lower(interaction_purpose_descrip) like '%phone%cant%'
+        or lower(interaction_purpose_descrip) like '%phone%no%'
+        or lower(interaction_purpose_descrip) like '%no%connect%'
+        or lower(interaction_purpose_descrip) like '%no%conect%'
+        or lower(interaction_purpose_descrip) like '%no%start%'
+        or lower(interaction_purpose_descrip) like '%equip%'
+        or lower(interaction_purpose_descrip) like '%intermit%'
+        or lower(interaction_purpose_descrip) like '%no%dat%'
+        or lower(interaction_purpose_descrip) like '%dat%serv%'
+        or lower(interaction_purpose_descrip) like '%int%data%'
+        or lower(interaction_purpose_descrip) like '%tech%'
+        or lower(interaction_purpose_descrip) like '%supp%'
+        or lower(interaction_purpose_descrip) like '%outage%'
+        or lower(interaction_purpose_descrip) like '%mass%'
+        or lower(interaction_purpose_descrip) like '%discon%warn%'
+        ) and (
+        lower(interaction_purpose_descrip) not like '%work%order%status%'
+        and lower(interaction_purpose_descrip) not like '%default%call%wrapup%'
+        and lower(interaction_purpose_descrip) not like '%bound%call%'
+        and lower(interaction_purpose_descrip) not like '%cust%first%'
+        and lower(interaction_purpose_descrip) not like '%audit%'
+        and lower(interaction_purpose_descrip) not like '%eq%code%'
+        and lower(interaction_purpose_descrip) not like '%downg%'
+        and lower(interaction_purpose_descrip) not like '%upg%'
+        and lower(interaction_purpose_descrip) not like '%vol%discon%'
+        and lower(interaction_purpose_descrip) not like '%discon%serv%'
+        and lower(interaction_purpose_descrip) not like '%serv%call%'
+        )
+        then interaction_id else null
+    end as techticket_flag,
+    cast(job_no_ojb as varchar) as truckroll_flag
+FROM interactions_fields2 a
+LEFT JOIN truckrolls_clean b
+    ON a.interaction_date = cast(create_dte_ojb as date) and cast(a.account_id as varchar) = cast(b.sub_acct_no_sbb as varchar)
+WHERE
+     interaction_purpose_descrip not in ('Work Order Status', 'Default Call Wrapup', 'G:outbound Calls', 'Eq: Cust. First', 'Eq: Audit', 'Eq: Code Error', 'Downgrade Service', 'Disconnect Service', 'Rt: Dowgrde Service', 'Cust Service Calls')
+    and (lower(interaction_purpose_descrip) like '%ppv%problem%'
+    or lower(interaction_purpose_descrip) like '%hsd%problem%'
+    or lower(interaction_purpose_descrip) like '%cable%problem%'
+    or lower(interaction_purpose_descrip) like '%tv%problem%'
+    or lower(interaction_purpose_descrip) like '%video%problem%'
+    or lower(interaction_purpose_descrip) like '%tel%problem%'
+    or lower(interaction_purpose_descrip) like '%phone%problem%'
+    or lower(interaction_purpose_descrip) like '%int%problem%'
+    or lower(interaction_purpose_descrip) like '%line%problem%'
+    or lower(interaction_purpose_descrip) like '%hsd%issue%'
+    or lower(interaction_purpose_descrip) like '%ppv%issue%'
+    or lower(interaction_purpose_descrip) like '%video%issue%'
+    or lower(interaction_purpose_descrip) like '%tel%issue%'
+    or lower(interaction_purpose_descrip) like '%phone%issue%'
+    or lower(interaction_purpose_descrip) like '%int%issue%'
+    or lower(interaction_purpose_descrip) like '%line%issue%'
+    or lower(interaction_purpose_descrip) like '%cable%issue%'
+    or lower(interaction_purpose_descrip) like '%tv%issue%'
+    or lower(interaction_purpose_descrip) like '%bloq%'
+    or lower(interaction_purpose_descrip) like '%slow%'
+    or lower(interaction_purpose_descrip) like '%slow%service%'
+    or lower(interaction_purpose_descrip) like '%service%tech%'
+    or lower(interaction_purpose_descrip) like '%tech%service%'
+    or lower(interaction_purpose_descrip) like '%no%service%'
+    or lower(interaction_purpose_descrip) like '%hsd%no%'
+    or lower(interaction_purpose_descrip) like '%hsd%slow%'
+    or lower(interaction_purpose_descrip) like '%hsd%intermit%'
+    or lower(interaction_purpose_descrip) like '%no%brows%'
+    or lower(interaction_purpose_descrip) like '%phone%cant%'
+    or lower(interaction_purpose_descrip) like '%phone%no%'
+    or lower(interaction_purpose_descrip) like '%no%connect%'
+    or lower(interaction_purpose_descrip) like '%no%conect%'
+    or lower(interaction_purpose_descrip) like '%no%start%'
+    or lower(interaction_purpose_descrip) like '%equip%'
+    or lower(interaction_purpose_descrip) like '%intermit%'
+    or lower(interaction_purpose_descrip) like '%no%dat%'
+    or lower(interaction_purpose_descrip) like '%dat%serv%'
+    or lower(interaction_purpose_descrip) like '%int%data%'
+    or lower(interaction_purpose_descrip) like '%tech%'
+    or lower(interaction_purpose_descrip) like '%supp%'
+    or lower(interaction_purpose_descrip) like '%outage%'
+    or lower(interaction_purpose_descrip) like '%mass%'
+    or lower(interaction_purpose_descrip) like '%discon%warn%'
+    ) and (
+    lower(interaction_purpose_descrip) not like '%work%order%status%'
+    and lower(interaction_purpose_descrip) not like '%default%call%wrapup%'
+    and lower(interaction_purpose_descrip) not like '%bound%call%'
+    and lower(interaction_purpose_descrip) not like '%cust%first%'
+    and lower(interaction_purpose_descrip) not like '%audit%'
+    and lower(interaction_purpose_descrip) not like '%eq%code%'
+    and lower(interaction_purpose_descrip) not like '%downg%'
+    and lower(interaction_purpose_descrip) not like '%upg%'
+    and lower(interaction_purpose_descrip) not like '%vol%discon%'
+    and lower(interaction_purpose_descrip) not like '%discon%serv%'
+    and lower(interaction_purpose_descrip) not like '%serv%call%'
+        )
+)
+
+, users_tickets as (
+SELECT 
+    *, 
+    case 
+        when techticket_flag is null and truckroll_flag is null then null
+        when techticket_flag is not null and truckroll_flag is null then interaction_id
+        when techticket_flag is null and truckroll_flag is not null then interaction_id
+        when techticket_flag is not null and truckroll_flag is not null then interaction_id
+    end)
+    as number_tickets,
+FROM users_tickets_pre1
+)
+
+, last_ticket as (
+SELECT 
+    account_id as last_account, 
+    first_value(interaction_date) over(partition by account_id, date_trunc('month', interaction_date) order by interaction_date desc) as last_interaction_date
+FROM users_tickets
+)
+
+, join_last_ticket as (
+SELECT
+    account_id, 
+    interaction_id, 
+    interaction_date, 
+    date_trunc('month', last_interaction_date) as interaction_month, 
+    last_interaction_date, 
+    date_add('day', -60, last_interaction_date) as window_day
+FROM users_tickets W
+INNER JOIN last_ticket L
+    ON W.account_id = L.last_account
+)
+
+, tickets_count as (
+SELECT 
+    interaction_month, 
+    account_id, 
+    count(distinct number_tickets) as tickets
+FROM join_last_ticket
+WHERE interaction_date between window_day and last_interaction_date
+GROUP BY 1, 2
+)
+
+, tickets_tier as (
+SELECT 
+    *,
+    case
+        when tickets = 1 then '1'
+        when tickets = 2 then '2'
+        when tickets >= 3 then '>3'
+    else null end as ticket_tier
+FROM tickets_count
+)
+
+--- ### Reiterative tickets flag
+
+, ticket_tier_flag as (
+SELECT 
+    F.*, 
+    ticket_tier
+FROM fmc_table_adj F
+LEFT JOIN tickets_tier I
+    ON cast(F.fix_s_att_account as varchar) = cast(I.account_id as varchar) and F.fix_s_dim_month = I.interaction_month
+)
+
+, final_fields as (
+SELECT
+    distinct fix_s_dim_month, -- month
+    fix_b_fla_tech, -- B_Final_TechFlag
+    fix_b_fla_fmc, -- B_FMCSegment
+    fix_b_fla_mixcodeadj, -- B_FMCType
+    fix_e_fla_tech, -- E_Final_TechFlag
+    fix_e_fla_fmc, -- E_FMCSegment
+    fix_e_fla_mixcodeadj, -- E_FMCType
+    -- b_final_tenure
+    -- e_final_tenure
+    fix_b_fla_tenure, -- B_FixedTenure
+    fix_e_fla_tenure, -- E_FixedTenure
+    -- finalchurnflag
+    -- fixedchurnflag
+    fix_s_fla_churntype, -- fixedchurntype
+    fix_s_fla_mainmovement, -- fixedmainmovement
+    -- waterfall_flag
+    -- mobile_activeeom
+    -- mobilechurnflag
+    ticket_tier,
+   -- finalaccount
+    fix_s_att_account, -- fixedaccount
+    records_per_user
+FROM ticket_tier_flag
+WHERE 
+  fix_s_fla_churnflag = '2. Fixed NonChurner'
+  and fix_e_att_active = 1
+)
+
+-- SELECT
+--     fix_s_dim_month, -- month
+--     fix_b_fla_tech, -- B_Final_TechFlag
+--     fix_b_fla_fmc, -- B_FMCSegment
+--     fix_b_fla_mixcodeadj, -- B_FMCType
+--     fix_e_fla_tech, -- E_Final_TechFlag
+--     fix_e_fla_fmc, -- E_FMCSegment
+--     fix_e_fla_mixcodeadj, -- E_FMCType
+--     -- b_final_tenure
+--     -- e_final_tenure
+--     fix_b_fla_tenure, -- B_FixedTenure
+--     fix_e_fla_tenure, -- E_FixedTenure
+--     -- finalchurnflag
+--     -- fixedchurnflag
+--     fix_s_fla_churntype, -- fixedchurntype
+--     fix_s_fla_mainmovement, -- fixedmainmovement
+--     -- waterfall_flag
+--     -- mobile_activeeom
+--     -- mobilechurnflag
+--     ticket_tier,
+--     count(distinct fix_s_att_account) as Total_Accounts,
+--     count(distinct fix_s_att_account) as Fixed_Accounts
+-- FROM final_fields
+-- GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
+
+--- ### Specific numbers
+
+-- SELECT
+--   count(distinct fix_s_att_account) as num_clients
+-- FROM final_fields
+-- WHERE ticket_tier = '1'
+
+SELECT 
+    distinct ticket_tier,
+    count(distinct fix_s_att_account) as num_clients
+FROM final_fields
+GROUP BY 1
