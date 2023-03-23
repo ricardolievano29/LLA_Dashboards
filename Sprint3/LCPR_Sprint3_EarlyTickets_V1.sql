48737dab0f4d314df8492273cf9ea0c1a464ddc9
juanbeltran852 <125683314+juanbeltran852@users.noreply.github.com>
2023-03-23 17:47:43 -0500

    Update LCPR_Sprint3_EarlyTickets_V1.sql

--- Sprint3/LCPR_Sprint3_EarlyTickets_V1.sql
+++ Sprint3/LCPR_Sprint3_EarlyTickets_V1.sql
@@ -1,6 +1,6 @@
 with parameters AS (
 -- Seleccionar el mes en que se desea realizar la corrida
-SELECT DATE_TRUNC('month',DATE('2023-01-01')) AS input_month
+SELECT DATE_TRUNC('month',DATE('2022-12-01')) AS input_month
 )
 ---New customer directly from the DNA
 ,new_customers_pre as (
@@ -14,7 +14,70 @@ SELECT (CAST(CAST(first_value(connect_dte_sbb) over (PARTITION BY sub_acct_no_sb
  ,new_customer as (   select date_trunc('month', fix_b_att_maxstart) as install_month,fix_b_att_maxstart,  fix_s_att_account from new_customers_pre )
 
 ,interactions as (
-select customer_id,interaction_start_time,date_trunc('month',date(interaction_start_time)) as interaction_start_month from "lcpr.stage.prod"."lcpr_interactions_csg" where interaction_purpose_descrip in ( 'Antivirus Calls', 'Bloq Red Wifi', 'Bloqueo Red Wifi', 'Cable Card Install', 'Cable Problem', 'Call Fwd Activaci??n', 'Call Fwd Activacin', 'Call Fwd Activacion', 'Call Fwd Desactivar', 'Cambio Ssid', 'Ci: Cable Card Req', 'Commercial Accounts', 'Configuration', 'Cs: Change Name(fco)', 'Cs: Offer Correction', 'Cs: Portability', 'Email Issues', 'Eq: Deprogrammed', 'Eq: Intermittence', 'Eq: Lost', 'Eq: Lost/Stolen', 'Eq: No Conection', 'Eq: No Start', 'Eq: Not Working', 'Eq: Notif Letter', 'Eq: Pixels', 'Eq: Ref. By Csr', 'Eq: Replace Remote', 'Eq: Return Equip.', 'Eq: Up/Dwn/Side', 'Eq: Up/Side', 'Fast Busy Tone', 'Functions Oriented', 'G:disconect Warning', 'G:follow Up', 'G:order Entry Error', 'Headend Issues', 'Headend Power Outage', 'HSD Intermittent', 'HSD Issues', 'HSD No Browsing', 'HSD Problem', 'HSD Slow Service', 'Ibbs-Ip Issues', 'Integra 5', 'Internet Calls', 'Lnp Complete', 'Lnp In Process', 'Lnp Not Complete', 'Mcafee', 'Mi Liberty Problem', 'Nc Busy Tone', 'Nc Cancel Job', 'Nc Message V. Mail', 'Nc No Answer', 'Nc Ok Cust Confirmed', 'Nc Rescheduled', 'Nc Wrong Phone No.', 'No Browse', 'No Retent Relate', 'No Retention Call', 'No Service All', 'Non Pay', 'Np: Restard Svc Only', 'Nret- Contract', 'Nret- Diss Cust Serv', 'Nret- Equipment', 'Nret- Moving', 'Nret- No Facilities', 'Outages', 'Phone Cant Make Call', 'Phone Cant Recv Call', 'Phone No Tone', 'PPV Order', 'PPV/Vod Problem', 'Reconnection', 'Refered Same Day', 'Restard Svc Only', 'Restart 68-69 Days', 'Restart Svc Only', 'Ret-Serv Education', 'Ret-Sidegrade', 'Ret-Upgrade', 'Retained Customer', 'Retent Effort Call', 'Retention', 'Return Mail', 'Rt: Dowgrde Convert', 'Rt: Dowgrde Premium', 'Schd Appiont 4 Tech', 'Self-Inst Successful', 'Self-Install', 'Self-Install N/A', 'Self-Int Rejected', 'Service Techs', 'Sl: Advanced Prod.', 'Sl: Install/Cos Conf', 'Sl: Outbound Sale', 'Sl: Product Info', 'Sl: Restart', 'Sl: Upg Addon/Tiers', 'Sl: Upg Events', 'Sl: Upg Service', 'Sl: Upgrade Tn', 'Sol Contrasena Wifi', 'Solicitud Contrasena', 'Solicitud Num Cuent', 'Solicitud Num Cuenta', 'Sp: Aee-No Liberty', 'Vd: Tech.Service', 'Video Issues', 'Video Problem', 'Video Programming', 'Voice Issues', 'Voice Outages', 'Wifi Password', 'Work Order Status') and date_trunc('month',date(interaction_start_time)) between (select input_month from parameters) and (select input_month + interval '2' month from parameters)
+select 
+    customer_id, 
+    interaction_start_time, 
+    date_trunc('month',date(interaction_start_time)) as interaction_start_month 
+from "lcpr.stage.prod"."lcpr_interactions_csg" 
+where 
+    interaction_purpose_descrip not in ('Work Order Status', 'Default Call Wrapup', 'G:outbound Calls', 'Eq: Cust. First', 'Eq: Audit', 'Eq: Code Error', 'Downgrade Service', 'Disconnect Service', 'Rt: Dowgrde Service', 'Cust Service Calls')
+    and (lower(interaction_purpose_descrip) like '%ppv%problem%'
+    or lower(interaction_purpose_descrip) like '%hsd%problem%'
+    or lower(interaction_purpose_descrip) like '%cable%problem%'
+    or lower(interaction_purpose_descrip) like '%tv%problem%'
+    or lower(interaction_purpose_descrip) like '%video%problem%'
+    or lower(interaction_purpose_descrip) like '%tel%problem%'
+    or lower(interaction_purpose_descrip) like '%phone%problem%'
+    or lower(interaction_purpose_descrip) like '%int%problem%'
+    or lower(interaction_purpose_descrip) like '%line%problem%'
+    or lower(interaction_purpose_descrip) like '%hsd%issue%'
+    or lower(interaction_purpose_descrip) like '%ppv%issue%'
+    or lower(interaction_purpose_descrip) like '%video%issue%'
+    or lower(interaction_purpose_descrip) like '%tel%issue%'
+    or lower(interaction_purpose_descrip) like '%phone%issue%'
+    or lower(interaction_purpose_descrip) like '%int%issue%'
+    or lower(interaction_purpose_descrip) like '%line%issue%'
+    or lower(interaction_purpose_descrip) like '%cable%issue%'
+    or lower(interaction_purpose_descrip) like '%tv%issue%'
+    or lower(interaction_purpose_descrip) like '%bloq%'
+    or lower(interaction_purpose_descrip) like '%slow%'
+    or lower(interaction_purpose_descrip) like '%slow%service%'
+    or lower(interaction_purpose_descrip) like '%service%tech%'
+    or lower(interaction_purpose_descrip) like '%tech%service%'
+    or lower(interaction_purpose_descrip) like '%no%service%'
+    or lower(interaction_purpose_descrip) like '%hsd%no%'
+    or lower(interaction_purpose_descrip) like '%hsd%slow%'
+    or lower(interaction_purpose_descrip) like '%hsd%intermit%'
+    or lower(interaction_purpose_descrip) like '%no%brows%'
+    or lower(interaction_purpose_descrip) like '%phone%cant%'
+    or lower(interaction_purpose_descrip) like '%phone%no%'
+    or lower(interaction_purpose_descrip) like '%no%connect%'
+    or lower(interaction_purpose_descrip) like '%no%conect%'
+    or lower(interaction_purpose_descrip) like '%no%start%'
+    or lower(interaction_purpose_descrip) like '%equip%'
+    or lower(interaction_purpose_descrip) like '%intermit%'
+    or lower(interaction_purpose_descrip) like '%no%dat%'
+    or lower(interaction_purpose_descrip) like '%dat%serv%'
+    or lower(interaction_purpose_descrip) like '%int%data%'
+    or lower(interaction_purpose_descrip) like '%tech%'
+    or lower(interaction_purpose_descrip) like '%supp%'
+    or lower(interaction_purpose_descrip) like '%outage%'
+    or lower(interaction_purpose_descrip) like '%mass%'
+    or lower(interaction_purpose_descrip) like '%discon%warn%'
+    ) and (
+    lower(interaction_purpose_descrip) not like '%work%order%status%'
+    and lower(interaction_purpose_descrip) not like '%default%call%wrapup%'
+    and lower(interaction_purpose_descrip) not like '%bound%call%'
+    and lower(interaction_purpose_descrip) not like '%cust%first%'
+    and lower(interaction_purpose_descrip) not like '%audit%'
+    and lower(interaction_purpose_descrip) not like '%eq%code%'
+    and lower(interaction_purpose_descrip) not like '%downg%'
+    and lower(interaction_purpose_descrip) not like '%upg%'
+    and lower(interaction_purpose_descrip) not like '%vol%discon%'
+    and lower(interaction_purpose_descrip) not like '%discon%serv%'
+    and lower(interaction_purpose_descrip) not like '%serv%call%'
+        )
+    and date_trunc('month',date(interaction_start_time)) between (select input_month from parameters) and (select input_month + interval '2' month from parameters)
 )
 
 ,new_customer_interactions_info AS (
@@ -24,5 +87,6 @@ FROM new_customer A LEFT JOIN interactions B ON A.fix_s_att_account = cast(B.cus
     --AND interaction_type = 'Technical'
 -- GROUP BY 1,2,3
 )
+
 select date_add('month', 0,install_month), count(distinct early_interaction_flag) as opd_s_mes_uni_early_tickets
 ,count(distinct fix_s_att_account) as fixed_Account from new_customer_interactions_info group by 1 order by 1
