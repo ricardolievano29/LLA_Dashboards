bab97e84aeacb05e1588f764634fc565f813d935
juanbeltran852 <125683314+juanbeltran852@users.noreply.github.com>
2023-04-18 21:08:12 -0500

    Update LCPR_Sprint3_OutlierInstalls_V2.sql

--- LCPR/Sprint3_OutlierInstalls/LCPR_Sprint3_OutlierInstalls_V2.sql
+++ LCPR/Sprint3_OutlierInstalls/LCPR_Sprint3_OutlierInstalls_V2.sql
@@ -3,28 +3,29 @@
 WITH
 
 parameters as (
-SELECT date_trunc('month', date('2023-01-01')) AS input_month
+SELECT date_trunc('month', date('2023-03-01')) AS input_month
 )
 
---- --- --- FMC table
+--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+--- --- --- --- --- --- --- --- --- --- --- FMC Table --- --- --- --- --- --- --- --- --- --- ---
+--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
 , fmc_table as (
 SELECT
     *
-FROM "db_stage_dev"."lcpr_fmc_table_dec_mar23" --- Make sure to set the month accordindly to the input month of parameters
-UNION ALL (SELECT * FROM "db_stage_dev"."lcpr_fmc_table_jan_mar23")
-UNION ALL (SELECT * FROM "db_stage_dev"."lcpr_fmc_table_feb_mar23")
--- WHERE 
-    -- fmc_s_dim_month = (SELECT input_month FROM parameters)
+FROM "lla_cco_lcpr_ana_prod"."lcpr_fmc_churn_dev"
+WHERE 
+    fmc_s_dim_month = (SELECT input_month FROM parameters)
 )
 
 , repeated_accounts as (
 SELECT 
     fmc_s_dim_month, 
-    fix_s_att_account, --- Operational Drivers are focused just in Fixed dynamics, not Mobile. I don't take the FMC account because sometimes in concatenates Fixed and Mobile accounts ids, which alters the results when joining with other bases using the account id.
+    fix_s_att_account,
     count(*) as records_per_user
 FROM fmc_table
 WHERE 
-    fix_s_att_account is not null --- Making sure that we are focusing just in Fixed.
+    fix_s_att_account is not null
 GROUP BY 1, 2
 ORDER BY 3 desc
 )
@@ -38,8 +39,10 @@ LEFT JOIN repeated_accounts R
     ON F.fix_s_att_account = R.fix_s_att_account and F.fmc_s_dim_month = R.fmc_s_dim_month
 )
 
+--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+--- --- --- --- --- --- --- --- --- --- --- New customers --- --- --- --- --- --- --- --- --- --- ---
+--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 
---- --- --- New customers
 , new_customers_pre as (
 SELECT 
     cast(cast(first_value(connect_dte_sbb) over (partition by sub_acct_no_sbb order by date(dt) desc) as timestamp) as date) as fix_b_att_maxstart,
@@ -61,7 +64,9 @@ SELECT
 FROM new_customers_pre
 )
 
---- ### ### ### Outlier Installs
+--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+--- --- --- --- --- --- --- --- --- --- --- Outlier Installs --- --- --- --- --- --- --- --- --- --- ---
+--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 
 , installations as (
 SELECT
@@ -87,6 +92,10 @@ LEFT JOIN installations b
     ON cast(a.fix_s_att_account as varchar) = cast(b.account_id as varchar)
 )
 
+--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+--- --- --- --- --- --- --- --- --- --- --- Final flag --- --- --- --- --- --- --- --- --- --- ---
+--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
 , flag_outlier_installs as (
 SELECT
     F.*, 
@@ -101,41 +110,22 @@ LEFT JOIN new_installations I
 WHERE
     fmc_s_dim_month = (SELECT input_month FROM parameters)
     and fix_e_att_active = 1 
-
 )
 
+, final_table as (
 SELECT 
     fmc_s_dim_month as opd_s_dim_month,
-    fmc_e_fla_tech as odr_e_fla_final_tech, -- E_Final_Tech_Flag, 
-    fmc_e_fla_fmcsegment as odr_e_fla_fmc_segment, -- E_FMC_Segment, 
-    fmc_e_fla_fmc as odr_e_fla_fmc_type, -- E_FMCType, 
-    case 
+    fmc_e_fla_tech as odr_e_fla_final_tech,
+    fmc_e_fla_fmcsegment as odr_e_fla_fmc_segment,
+    fmc_e_fla_fmc as odr_e_fla_fmc_type,
+    case --- Making sure the tenures fit with the dashboard ones
         when fmc_e_fla_tenure = 'Early Tenure' then 'Early-Tenure'
         when fmc_e_fla_tenure = 'Mid Tenure' then 'Mid-Tenure'
         when fmc_e_fla_tenure = 'Late Tenure' then 'Late-Tenure'
     end as odr_e_fla_final_tenure, ---E_FinalTenureSegment,
-    count(distinct fix_s_att_account) as odr_s_mes_active_base, -- as activebase, 
-    -- sales_channel, 
-    count(distinct new_sales_flag) as opd_s_mes_sales, -- sum(monthsale_flag) as Sales, 
-    -- sum(SoftDx_Flag) as Soft_Dx, 
-    -- sum (NeverPaid_Flag) as NeverPaid,
+    count(distinct fix_s_att_account) as odr_s_mes_active_base,
+    count(distinct new_sales_flag) as opd_s_mes_sales,
     count(distinct outlier_install_flag) as opd_s_mes_long_installs
-    -- sum (increase_flag) as MRC_Increases, 
-    -- sum (no_plan_change_flag) as NoPlan_Changes, 
-    -- sum(mountingbill_flag) as MountingBills, 
-    -- count(distinct early_ticket_flag) as EarlyTickets
-    -- Sales_Month, 
-    -- Install_Month, 
-    -- Ticket_Month, 
-    -- count(distinct F_SalesFlag) Unique_Sales, 
-    -- count(distinct soft_dx_flag) as opd_s_mes_uni_softdx
-    -- count(distinct day_85) as opd_s_mes_uni_never_paid,
-    -- count(distinct F_LongInstallFlag) Unique_LongInstall,
-    -- count(distinct mrc_increase_flag) as opd_s_mes_uni_mrcincrease,
-    -- count(distinct no_plan_change) as opd_s_mes_uni_noplan_changes,
-    -- count(distinct mounting_bill_flag) as opd_s_mes_uni_moun_gbills, 
-    -- count(distinct early_ticket_flag) as opd_s_mes_uni_early_tickets
-    -- count(distinct billing_claim_flag) as opd_s_mes_uni_bill_claim
 FROM flag_outlier_installs
 WHERE 
     fmc_s_fla_churnflag != 'Fixed Churner' 
@@ -143,3 +133,18 @@ WHERE
     and fix_s_fla_mainmovement != '6.Null last day'
 GROUP BY 1, 2, 3, 4, 5
 ORDER BY 1, 2, 3, 4, 5
+)
+
+-- SELECT * FROM final_table
+
+--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+--- --- --- --- --- --- --- --- --- --- --- Tests --- --- --- --- --- --- --- --- --- --- ---
+--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+--- ### Outlier installs ###
+
+SELECT
+    sum(opd_s_mes_long_installs) as outlier_installs, 
+    sum(opd_s_mes_sales) as sales_base, 
+    cast(sum(opd_s_mes_long_installs) as double)/cast(sum(opd_s_mes_sales) as double) as KPI
+FROM final_table
