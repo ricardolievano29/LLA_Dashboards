9ab637c0262cbcf42c550f2cb8dd3e0bb5c7b927
juanbeltran852 <125683314+juanbeltran852@users.noreply.github.com>
2023-04-24 22:44:03 -0500

    Create LCPR_Sprint5_Nodes_Ticket_Density_VF.sql

---
+++ LCPR/LCPR_OpDrivers/Singular_KPIs/LCPR_Sprint5_Nodes_Ticket_Density_VF.sql
@@ -0,0 +1,201 @@
+--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+--- --- --- --- ##### LCPR - SPRINT 5 - OPERATIONAL DRIVERS - NODES TICKET DENSITY ##### --- --- --- ---
+--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+WITH
+
+parameters as (SELECT date_trunc('month', date('2023-01-01')) as input_month)
+
+--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+--- --- --- --- --- --- --- --- --- --- --- DNA --- --- --- --- --- --- --- --- --- --- ---
+--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+, customers_dna_pre as (
+SELECT 
+    sub_acct_no_sbb, 
+    date(dt) as dt, 
+    bridger_addr_hse
+FROM "lcpr.stage.prod"."insights_customer_services_rates_lcpr"
+WHERE 
+    date_trunc('month', date(dt)) = (SELECT input_month FROM parameters)
+    and cust_typ_sbb = 'RES'
+)
+
+, customers_dna as (
+SELECT 
+    sub_acct_no_sbb, 
+    bridger_addr_hse,
+    min(dt) as first_dt, 
+    max(dt) as last_dt
+FROM customers_dna_pre
+GROUP BY sub_acct_no_sbb, bridger_addr_hse
+)
+
+--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+--- --- --- --- --- --- --- --- --- --- --- Interactions and truckrolls --- --- --- --- --- --- --- --- --- --- ---
+--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+
+, clean_interaction_time as (
+SELECT *
+FROM "lcpr.stage.prod"."lcpr_interactions_csg"
+WHERE
+    cast(interaction_start_time as varchar) != ' ' 
+    and interaction_start_time is not null
+    and date_trunc('month', date(interaction_start_time)) = (SELECT input_month FROM parameters)
+    and account_type = 'RES'
+)
+
+, interactions_fields as (
+SELECT
+    *, 
+    cast(substr(cast(interaction_start_time as varchar), 1, 10) as date) as interaction_date, 
+    date_trunc('month', cast(substr(cast(interaction_start_time as varchar), 1, 10) as date)) as month
+FROM clean_interaction_time
+)
+
+, interactions_not_repeated as (
+SELECT
+    first_value(interaction_id) OVER(PARTITION BY account_id, interaction_date, interaction_channel, interaction_agent_id, interaction_purpose_descrip ORDER BY interaction_date DESC) AS interaction_id2
+FROM interactions_fields
+)
+
+, interactions_fields2 as (
+SELECT *
+FROM interactions_not_repeated a
+LEFT JOIN interactions_fields b
+    ON a.interaction_id2 = b.interaction_id
+)
+
+, truckrolls as (
+SELECT 
+    create_dte_ojb, 
+    job_no_ojb, 
+    sub_acct_no_sbb
+FROM "lcpr.stage.dev"."truckrolls" 
+)
+
+, full_interactions as (
+SELECT 
+    *, 
+    case 
+    
+        when create_dte_ojb is not null then 'truckroll'
+    
+        when (
+        lower(interaction_purpose_descrip) like '%ppv%problem%'
+        or lower(interaction_purpose_descrip) like '%hsd%problem%'
+        or lower(interaction_purpose_descrip) like '%cable%problem%'
+        or lower(interaction_purpose_descrip) like '%tv%problem%'
+        or lower(interaction_purpose_descrip) like '%video%problem%'
+        or lower(interaction_purpose_descrip) like '%tel%problem%'
+        or lower(interaction_purpose_descrip) like '%phone%problem%'
+        or lower(interaction_purpose_descrip) like '%int%problem%'
+        or lower(interaction_purpose_descrip) like '%line%problem%'
+        or lower(interaction_purpose_descrip) like '%hsd%issue%'
+        or lower(interaction_purpose_descrip) like '%ppv%issue%'
+        or lower(interaction_purpose_descrip) like '%video%issue%'
+        or lower(interaction_purpose_descrip) like '%tel%issue%'
+        or lower(interaction_purpose_descrip) like '%phone%issue%'
+        or lower(interaction_purpose_descrip) like '%int%issue%'
+        or lower(interaction_purpose_descrip) like '%line%issue%'
+        or lower(interaction_purpose_descrip) like '%cable%issue%'
+        or lower(interaction_purpose_descrip) like '%tv%issue%'
+        or lower(interaction_purpose_descrip) like '%bloq%'
+        or lower(interaction_purpose_descrip) like '%slow%'
+        or lower(interaction_purpose_descrip) like '%slow%service%'
+        or lower(interaction_purpose_descrip) like '%service%tech%'
+        or lower(interaction_purpose_descrip) like '%tech%service%'
+        or lower(interaction_purpose_descrip) like '%no%service%'
+        or lower(interaction_purpose_descrip) like '%hsd%no%'
+        or lower(interaction_purpose_descrip) like '%hsd%slow%'
+        or lower(interaction_purpose_descrip) like '%hsd%intermit%'
+        or lower(interaction_purpose_descrip) like '%no%brows%'
+        or lower(interaction_purpose_descrip) like '%phone%cant%'
+        or lower(interaction_purpose_descrip) like '%phone%no%'
+        or lower(interaction_purpose_descrip) like '%no%connect%'
+        or lower(interaction_purpose_descrip) like '%no%conect%'
+        or lower(interaction_purpose_descrip) like '%no%start%'
+        or lower(interaction_purpose_descrip) like '%equip%'
+        or lower(interaction_purpose_descrip) like '%intermit%'
+        or lower(interaction_purpose_descrip) like '%no%dat%'
+        or lower(interaction_purpose_descrip) like '%dat%serv%'
+        or lower(interaction_purpose_descrip) like '%int%data%'
+        or lower(interaction_purpose_descrip) like '%tech%'
+        or lower(interaction_purpose_descrip) like '%supp%'
+        or lower(interaction_purpose_descrip) like '%outage%'
+        or lower(interaction_purpose_descrip) like '%mass%'
+        or lower(interaction_purpose_descrip) like '%discon%warn%'
+        ) and (
+        lower(interaction_purpose_descrip) not like '%work%order%status%'
+        and lower(interaction_purpose_descrip) not like '%default%call%wrapup%'
+        and lower(interaction_purpose_descrip) not like '%bound%call%'
+        and lower(interaction_purpose_descrip) not like '%cust%first%'
+        and lower(interaction_purpose_descrip) not like '%audit%'
+        and lower(interaction_purpose_descrip) not like '%eq%code%'
+        and lower(interaction_purpose_descrip) not like '%downg%'
+        and lower(interaction_purpose_descrip) not like '%upg%'
+        and lower(interaction_purpose_descrip) not like '%vol%discon%'
+        and lower(interaction_purpose_descrip) not like '%discon%serv%'
+        and lower(interaction_purpose_descrip) not like '%serv%call%'
+        )
+        then 'tech_call'
+        
+        else null
+        
+        end as interaction_type
+        
+FROM interactions_fields2 a
+FULL OUTER JOIN truckrolls b
+    ON a.interaction_date = cast(create_dte_ojb as date) and cast(a.account_id as varchar) = cast(b.sub_acct_no_sbb as varchar)
+WHERE
+    interaction_purpose_descrip not in ('Work Order Status', 'Default Call Wrapup', 'G:outbound Calls', 'Eq: Cust. First', 'Eq: Audit', 'Eq: Code Error', 'Downgrade Service', 'Disconnect Service', 'Rt: Dowgrde Service', 'Cust Service Calls')
+)
+
+
+--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+--- --- --- --- --- --- --- --- --- --- --- Nodes ticket density --- --- --- --- --- --- --- --- --- --- ---
+--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+, join_interactions as (
+SELECT
+    a.*, 
+    b.*, 
+    case when interaction_type in ('tech_call', 'truckroll') then interaction_id else null end as ticket_flag
+FROM full_interactions a
+LEFT JOIN customers_dna b
+    ON cast(a.account_id as varchar) = cast(b.sub_acct_no_sbb as varchar)
+)
+
+, account_panel as (
+SELECT
+    distinct account_id, 
+    bridger_addr_hse as node_id, 
+    max(ticket_flag) as ticket_flag
+FROM join_interactions
+GROUP BY 1, 2
+)
+
+, nodes_panel as (
+SELECT
+    distinct node_id,
+    count(distinct account_id) as cust_in_node, 
+    count(distinct case when ticket_flag is not null then account_id else null end) as cust_w_tickets_in_node
+FROM account_panel
+GROUP BY 1
+)
+
+, final as (
+SELECT 
+    node_id, 
+    cust_in_node, 
+    cust_w_tickets_in_node, 
+    case when cast(cust_w_tickets_in_node as double)/cast(cust_in_node as double) > 0.06 then node_id else null end as nodes_ticket_density_above_6pct
+FROM nodes_panel
+-- WHERE cust_in_node > 30
+)
+
+SELECT
+    count(distinct nodes_ticket_density_above_6pct) as nodes_w_ticket_density_above_6pct, 
+    count(distinct node_id) as total_nodes, 
+    cast(count(distinct nodes_ticket_density_above_6pct) as double)/cast(count(distinct node_id) as double) as KPI
+FROM final
