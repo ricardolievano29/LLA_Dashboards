83238f7b7de7c9408bb49502d955f2f14236fac1
juanbeltran852 <125683314+juanbeltran852@users.noreply.github.com>
2023-04-25 13:07:56 -0500

    Create LCPR_Sprint3_EarlyTickets_V6.sql

---
+++ LCPR/LCPR_OpDrivers/Old/LCPR_Sprint3_EarlyTickets_V6.sql
@@ -0,0 +1,290 @@
+--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+--- --- --- --- ##### LCPR - SPRINT 3 - OPERATIONAL DRIVERS - EARLY TICKETS ##### --- --- --- ---
+--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+WITH
+
+parameters as (
+SELECT date_trunc('month', date('2023-02-01')) as input_month
+)
+
+--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+--- --- --- --- --- --- --- --- --- --- --- Interactions and truckrolls --- --- --- --- --- --- --- ---
+--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+, clean_interaction_time as (
+SELECT *
+FROM "lcpr.stage.prod"."lcpr_interactions_csg"
+WHERE
+    cast(interaction_start_time as varchar) != ' ' 
+    and interaction_start_time is not null
+    and date_trunc('month', date(interaction_start_time)) between ((SELECT input_month FROM parameters) - interval '2' month) and (SELECT input_month FROM parameters) 
+    and account_type = 'RES'
+)
+
+, interactions_fields as (
+SELECT
+    *, 
+    cast(substr(cast(interaction_start_time as varchar), 1, 10) as date) as interaction_date, 
+    date_trunc('month', cast(substr(cast(interaction_start_time as varchar), 1, 10) as date)) as month
+FROM clean_interaction_time
+)
+
+, interactions_not_repeated as (
+SELECT
+    first_value(interaction_id) OVER(PARTITION BY account_id, interaction_date, interaction_channel, interaction_agent_id, interaction_purpose_descrip ORDER BY interaction_date DESC) AS interaction_id2
+FROM interactions_fields
+)
+
+, interactions_fields2 as (
+SELECT *
+FROM interactions_not_repeated a
+LEFT JOIN interactions_fields b
+    ON a.interaction_id2 = b.interaction_id
+)
+
+, truckrolls as (
+SELECT 
+    create_dte_ojb, 
+    job_no_ojb, 
+    sub_acct_no_sbb
+FROM "lcpr.stage.dev"."truckrolls" 
+WHERE 
+    date_trunc('month', date(create_dte_ojb)) between ((SELECT input_month FROM parameters) - interval '2' month) and ((SELECT input_month FROM parameters))
+)
+
+, full_interactions as (
+SELECT 
+    *, 
+    case 
+    
+        when create_dte_ojb is not null then 'truckroll'
+    
+        when (
+        lower(interaction_purpose_descrip) like '%ppv%problem%'
+        or lower(interaction_purpose_descrip) like '%hsd%problem%'
+        or lower(interaction_purpose_descrip) like '%cable%problem%'
+        or lower(interaction_purpose_descrip) like '%tv%problem%'
+        or lower(interaction_purpose_descrip) like '%video%problem%'
+        or lower(interaction_purpose_descrip) like '%tel%problem%'
+        or lower(interaction_purpose_descrip) like '%phone%problem%'
+        or lower(interaction_purpose_descrip) like '%int%problem%'
+        or lower(interaction_purpose_descrip) like '%line%problem%'
+        or lower(interaction_purpose_descrip) like '%hsd%issue%'
+        or lower(interaction_purpose_descrip) like '%ppv%issue%'
+        or lower(interaction_purpose_descrip) like '%video%issue%'
+        or lower(interaction_purpose_descrip) like '%tel%issue%'
+        or lower(interaction_purpose_descrip) like '%phone%issue%'
+        or lower(interaction_purpose_descrip) like '%int%issue%'
+        or lower(interaction_purpose_descrip) like '%line%issue%'
+        or lower(interaction_purpose_descrip) like '%cable%issue%'
+        or lower(interaction_purpose_descrip) like '%tv%issue%'
+        or lower(interaction_purpose_descrip) like '%bloq%'
+        or lower(interaction_purpose_descrip) like '%slow%'
+        or lower(interaction_purpose_descrip) like '%slow%service%'
+        or lower(interaction_purpose_descrip) like '%service%tech%'
+        or lower(interaction_purpose_descrip) like '%tech%service%'
+        or lower(interaction_purpose_descrip) like '%no%service%'
+        or lower(interaction_purpose_descrip) like '%hsd%no%'
+        or lower(interaction_purpose_descrip) like '%hsd%slow%'
+        or lower(interaction_purpose_descrip) like '%hsd%intermit%'
+        or lower(interaction_purpose_descrip) like '%no%brows%'
+        or lower(interaction_purpose_descrip) like '%phone%cant%'
+        or lower(interaction_purpose_descrip) like '%phone%no%'
+        or lower(interaction_purpose_descrip) like '%no%connect%'
+        or lower(interaction_purpose_descrip) like '%no%conect%'
+        or lower(interaction_purpose_descrip) like '%no%start%'
+        or lower(interaction_purpose_descrip) like '%equip%'
+        or lower(interaction_purpose_descrip) like '%intermit%'
+        or lower(interaction_purpose_descrip) like '%no%dat%'
+        or lower(interaction_purpose_descrip) like '%dat%serv%'
+        or lower(interaction_purpose_descrip) like '%int%data%'
+        or lower(interaction_purpose_descrip) like '%tech%'
+        or lower(interaction_purpose_descrip) like '%supp%'
+        or lower(interaction_purpose_descrip) like '%outage%'
+        or lower(interaction_purpose_descrip) like '%mass%'
+        or lower(interaction_purpose_descrip) like '%discon%warn%'
+        ) and (
+        lower(interaction_purpose_descrip) not like '%work%order%status%'
+        and lower(interaction_purpose_descrip) not like '%default%call%wrapup%'
+        and lower(interaction_purpose_descrip) not like '%bound%call%'
+        and lower(interaction_purpose_descrip) not like '%cust%first%'
+        and lower(interaction_purpose_descrip) not like '%audit%'
+        and lower(interaction_purpose_descrip) not like '%eq%code%'
+        and lower(interaction_purpose_descrip) not like '%downg%'
+        and lower(interaction_purpose_descrip) not like '%upg%'
+        and lower(interaction_purpose_descrip) not like '%vol%discon%'
+        and lower(interaction_purpose_descrip) not like '%discon%serv%'
+        and lower(interaction_purpose_descrip) not like '%serv%call%'
+        )
+        then 'tech_call'
+        
+        else null
+        
+        end as interaction_type
+        
+FROM interactions_fields2 a
+FULL OUTER JOIN truckrolls b
+    ON a.interaction_date = cast(create_dte_ojb as date) and cast(a.account_id as varchar) = cast(b.sub_acct_no_sbb as varchar)
+WHERE
+    interaction_purpose_descrip not in ('Work Order Status', 'Default Call Wrapup', 'G:outbound Calls', 'Eq: Cust. First', 'Eq: Audit', 'Eq: Code Error', 'Downgrade Service', 'Disconnect Service', 'Rt: Dowgrde Service', 'Cust Service Calls')
+)
+
+--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+--- --- --- --- --- --- --- --- --- --- --- FMC Table --- --- --- --- --- --- --- --- --- --- ---
+--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+, fmc_table as (
+SELECT
+    *
+FROM "lla_cco_lcpr_ana_prod"."lcpr_fmc_churn_dev"
+WHERE 
+    fmc_s_dim_month = (SELECT input_month FROM parameters)
+)
+
+, repeated_accounts as (
+SELECT 
+    fmc_s_dim_month, 
+    fix_s_att_account,
+    count(*) as records_per_user
+FROM fmc_table
+WHERE 
+    fix_s_att_account is not null
+GROUP BY 1, 2
+ORDER BY 3 desc
+)
+
+, fmc_table_adj as (
+SELECT 
+    F.*,
+    records_per_user
+FROM fmc_table F
+LEFT JOIN repeated_accounts R
+    ON F.fix_s_att_account = R.fix_s_att_account and F.fmc_s_dim_month = R.fmc_s_dim_month
+)
+
+--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+--- --- --- --- --- --- --- --- --- --- --- New customers --- --- --- --- --- --- --- --- --- --- ---
+--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+, new_customers_pre as (
+SELECT
+    cast(cast(first_value(connect_dte_sbb) over (partition by sub_acct_no_sbb order by DATE(dt) DESC) as timestamp) as date) as fix_b_att_maxstart,   
+    SUB_ACCT_NO_SBB as fix_s_att_account 
+FROM "lcpr.stage.prod"."insights_customer_services_rates_lcpr" 
+WHERE 
+    play_type != '0P'
+    and cust_typ_sbb = 'RES' 
+    and date_trunc('month', date(CONNECT_DTE_SBB)) between ((SELECT input_month FROM parameters) - interval '2' month) and (SELECT input_month FROM parameters)
+ORDER BY 1
+)
+    
+, new_customers2m as (   
+SELECT 
+    date_trunc('month', fix_b_att_maxstart) as install_month, 
+    fix_b_att_maxstart,  
+    fix_s_att_account as new_sales2m_flag,
+    fix_s_att_account
+FROM new_customers_pre
+WHERE date_trunc('month', date(fix_b_att_maxstart)) = ((SELECT input_month FROM parameters) - interval '2' month)
+)
+
+, new_customers as (   
+SELECT 
+    date_trunc('month', fix_b_att_maxstart) as install_month, 
+    fix_b_att_maxstart,  
+    fix_s_att_account as new_sales_flag,
+    fix_s_att_account
+FROM new_customers_pre
+WHERE date_trunc('month', date(fix_b_att_maxstart)) = (SELECT input_month FROM parameters)
+)
+
+--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+--- --- --- --- --- --- --- --- --- --- --- Early tickets --- --- --- --- --- --- --- --- --- --- ---
+--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+, relevant_interactions as (
+SELECT
+    customer_id, 
+    interaction_id, 
+    job_no_ojb,
+    interaction_type,
+    min(interaction_date) as min_interaction_date, 
+    min(date_trunc('month', date(interaction_date))) as interaction_start_month 
+FROM full_interactions
+GROUP BY 1, 2, 3, 4
+)
+
+, early_tickets AS (
+SELECT 
+    A.fix_s_att_account, 
+    new_sales2m_flag,
+    install_month, 
+    interaction_start_month, 
+    fix_b_att_maxstart,
+    case when date_diff('week', date(fix_b_att_maxstart), date(min_interaction_date)) <= 7 then fix_s_att_account else null end as early_ticket_flag
+FROM new_customers2m A 
+LEFT JOIN relevant_interactions B 
+    ON cast(A.fix_s_att_account as varchar) = cast(B.customer_id as varchar)
+WHERE interaction_type in ('tech_call', 'truckroll')
+)
+
+--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+--- --- --- --- --- --- --- --- --- --- --- Final flags --- --- --- --- --- --- --- --- --- --- ---
+--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+, flag_new_customers as (
+SELECT
+    F.*, 
+    new_sales_flag
+FROM fmc_table_adj F
+LEFT JOIN new_customers I
+    ON cast(F.fix_s_att_account as varchar) = cast(I.fix_s_att_account as varchar)
+WHERE
+    fmc_s_dim_month = (SELECT input_month FROM parameters)
+    and fix_e_att_active = 1
+)
+
+, flag_early_tickets as (
+SELECT
+    F.*, 
+    early_ticket_flag
+FROM flag_new_customers F
+LEFT JOIN early_tickets I
+    ON cast(F.fix_s_att_account as varchar) = cast(I.fix_s_att_account as varchar)
+)
+
+
+, final_table as (
+SELECT 
+    fmc_s_dim_month as opd_s_dim_month,
+    fmc_e_fla_tech as odr_e_fla_final_tech,
+    fmc_e_fla_fmcsegment as odr_e_fla_fmc_segment,
+    fmc_e_fla_fmc as odr_e_fla_fmc_type,
+    case 
+        when fmc_e_fla_tenure = 'Early Tenure' then 'Early-Tenure'
+        when fmc_e_fla_tenure = 'Mid Tenure' then 'Mid-Tenure'
+        when fmc_e_fla_tenure = 'Late Tenure' then 'Late-Tenure'
+    end as odr_e_fla_final_tenure,
+    count(distinct fix_s_att_account) as odr_s_mes_active_base,
+    count(distinct new_sales_flag) as opd_s_mes_sales,
+    count(distinct early_ticket_flag) as EarlyTickets
+FROM flag_early_tickets
+WHERE 
+    fmc_s_fla_churnflag != 'Fixed Churner' 
+    and fmc_s_fla_waterfall not in ('Downsell-Fixed Customer Gap', 'Fixed Base Exception', 'Churn Exception') 
+    and fix_s_fla_mainmovement != '6.Null last day'
+GROUP BY 1, 2, 3, 4, 5
+ORDER BY 1, 2, 3, 4, 5
+)
+
+-- SELECT * FROM final_table
+
+--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+--- --- --- --- --- --- --- --- --- --- --- Tests --- --- --- --- --- --- --- --- --- --- ---
+--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+SELECT 
+    sum(EarlyTickets) as early_tickets, 
+    sum(opd_s_mes_sales) as sales_base, 
+    cast(sum(EarlyTickets) as double)/cast(sum(opd_s_mes_sales) as double) as KPI
+FROM final_table
